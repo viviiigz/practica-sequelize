b3f18874a23ad106f042122af97456fcdc29a6ac
Viviana Gonzalez <vivigz06@gmail.com>
2025-06-22 14:46:03 -0300

    CONECTÉ LA BASE DE DATOOOSSS

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,56 @@
+// controllers/product.controller.js
+import Product from "../models/product.js";
+
+export const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.findAll();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    if (product) res.json(product);
+    else res.status(404).json({ message: "Producto no encontrado" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  try {
+    const [updated] = await Product.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (updated) {
+      const updatedProduct = await Product.findByPk(req.params.id);
+      res.json(updatedProduct);
+    } else {
+      res.status(404).json({ message: "Producto no encontrado" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const deleted = await Product.destroy({ where: { id: req.params.id } });
+    if (deleted) res.json({ message: "Producto eliminado" });
+    else res.status(404).json({ message: "Producto no encontrado" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
