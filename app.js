bb8f2bcf4be98d3fa319526138e3203674925121
Viviana Gonzalez <vivigz06@gmail.com>
2025-06-22 18:30:46 -0300

    importe userRouters

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import initDB from './src/config/db.js';
 import productRouters from './src/routers/product.routes.js'//HACER LO DE ROUTERRRR PUNTO 4
+import userRouters from './src/routers/user.routes.js'
 
 dotenv.config();//llama a la funcion config de dotenv
 //carfa todas las vaariables defibidas en el archivo .env 
@@ -15,7 +16,8 @@ app.use(express.json());
 
 // //routers
 app.get('/',(req,res)=> res.json({ok: 'true'}))
-app.use("/product",productRouters);
+app.use("/products",productRouters);
+app.use("/users", userRouters);
 
 app.listen (PORT, ()=> console.log("Server runnings on http://localhost:" + PORT));
 
